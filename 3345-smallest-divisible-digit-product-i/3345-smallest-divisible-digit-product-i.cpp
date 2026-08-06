07137648fc833161ff359f52028db4e1773b29c0
Shivani Singh <shivanis0339@gmail.com>
2026-08-06 21:52:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (73.4%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int productofdigit(int num){
+        int product=1;
+        while(num>0){
+            product*=num%10;
+            num/=10;
+        }
+        return product;
+    }
+    int smallestNumber(int n, int t) {
+        while(true){
+            if(productofdigit(n)%t==0){
+                return n;
+            }
+            n++;
+        }
+    }
+};
