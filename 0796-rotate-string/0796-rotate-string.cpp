25738366ebb791fb9ef6cbad852d7d54126916e3
Shivani Singh <shivanis0339@gmail.com>
2026-08-21 19:10:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (84.02%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()){
+            return false;
+        }
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            bool match=true;
+            for(int j=0;j<n;j++){
+                if((s[(i+j)%n])!=goal[j]){
+                    match=false;
+                    break;
+                }
+            }
+            if(match){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
