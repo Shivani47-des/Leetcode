ca1f917389a7f11fcc14a185c96c484fb2576014
Shivani Singh <shivanis0339@gmail.com>
2026-09-01 11:31:05 +0530

    Time: 0 ms (100%), Space: 12.6 MB (48.04%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,16 +4,21 @@ public:
         int low=0;
         int high=nums.size()-1;
 
-        while(low<high){
+        while(low<=high){
             int mid=low+(high-low)/2;
+            bool left=(mid==0 || nums[mid]>nums[mid-1]);
+            bool right=(mid==nums.size()-1 || nums[mid]>nums[mid+1]);
 
-            if(nums[mid]<nums[mid+1]){
+            if(left && right){
+                return mid;
+            }
+            if(mid<nums.size()-1 && nums[mid]<nums[mid+1]){
                 low=mid+1;
             }
             else{
-                high=mid;
+                high=mid-1;
             }
         }
-        return low;
+        return -1;
     }
 };
