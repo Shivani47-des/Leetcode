acdcf19a942946c416bb5d998a4521c7960e843d
Shivani Singh <shivanis0339@gmail.com>
2026-09-09 23:22:14 +0530

    Time: 684 ms (14.55%), Space: 25.4 MB (36.59%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -4,28 +4,23 @@ public:
         if(n <= 2)
             return 0;
 
-        // index i represents number (2*i + 1)
         int size = n / 2;
         vector<bool> isPrime(size, true);
 
-        // 1 is not prime
         isPrime[0] = false;
 
         for(int i = 3; i * i < n; i += 2) {
 
-            // index of i
             if(isPrime[i / 2]) {
 
-                // Start from i*i and consider only odd multiples
                 for(int j = i * i; j < n; j += 2 * i) {
                     isPrime[j / 2] = false;
                 }
             }
         }
 
-        int count = 1; // number 2
+        int count = 1; 
 
-        // Check only odd numbers
         for(int i = 3; i < n; i += 2) {
             if(isPrime[i / 2])
                 count++;
