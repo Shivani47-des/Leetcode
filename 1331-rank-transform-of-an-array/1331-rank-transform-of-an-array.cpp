f38b78791364133b48f032c8a3093bbf12776831
Shivani Singh <shivanis0339@gmail.com>
2026-09-23 00:49:29 +0530

    Time: 38 ms (53.62%), Space: 44 MB (48.39%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int>nums=arr;
+        sort(nums.begin(),nums.end());
+        unordered_map<int,int>mp;
+        int rank=1;
+        for(int x:nums){
+            if(mp.find(x)==mp.end()){
+                mp[x]=rank;
+                rank++;
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            arr[i]=mp[arr[i]];
+        }
+        return arr;
+    }
+};
